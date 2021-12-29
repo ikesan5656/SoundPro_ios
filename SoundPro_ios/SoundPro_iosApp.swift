0334a8f66e26b09ddb1e30e1eb5250b818a59991
池ノ谷優太 <you1207.ike@gmail.com>
2021-12-29 09:04:54 +0900

    XCode_Swift_Project FirstrCommit

---
+++ SoundPro_ios/SoundPro_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundPro_iosApp.swift
+//  SoundPro_ios
+//
+//  Created by 池ノ谷優太 on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SoundPro_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
